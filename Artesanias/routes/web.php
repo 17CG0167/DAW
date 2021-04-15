27a6276a85b5ff47e72c40e0b5d7e2d970c4edfb
17CG0167 <hector.skate99@gmail.com>
2021-04-14 22:00:59 -0600

    sistema de plantillas

--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+Route::get('/admin/usuarios', function () {
+    return view('admin.layouts.users');
+});
 Route::get('/contacto', function() {
     $contacto = "Hector Reyes";
     $valores = "10";
